da4e512f631ee94cf2eef70e40345d090476e0ac
yy <yanyang1116@gmail.com>
2025-07-04 14:34:42 +0800

    添加 Prettier 配置文件，更新 .prettierignore 文件以排除不必要的文件，升级依赖项 Prettier 至 3.6.2，并更新相关依赖版本，调整 tsconfig.json 文件格式。

---
+++ next-template/.prettierrc.cjs
@@ -0,0 +1,10 @@
+/**
+ * Prettier 代码格式化配置
+ * 测下来，此文件可以是 cjs ，但是不能是 mjs，当然 .prettierrc 也可以，可以被识别到
+ *
+ * @type {import('prettier').Config}
+ */
+module.exports = {
+  extends: "@nxlibs/prettier-config",
+  rules: {},
+};
