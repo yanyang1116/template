fa9cf7faf4311f55392e5d0f5be4eae060266b06
yy <yanyang1116@gmail.com>
2025-07-10 19:48:25 +0800

    chore(config): 新增 .editorconfig 文件以统一代码风格，更新 .prettierrc.cjs 文件注释，确保与 .editorconfig 一致，优化 Home 组件代码格式。

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,3 +1,9 @@
+/**
+ * @file
+ * 此类文件的修改，一定要重启编辑器，确保加载成功
+ *
+ * 这个文件需要和 .editorconfig 保持一致，以确保一些制表符，换行符的一致性
+ */
 const prettierConfig = require("@nxlibs/prettier-config");
 
 /**
@@ -5,6 +11,4 @@ const prettierConfig = require("@nxlibs/prettier-config");
  */
 module.exports = {
 	...prettierConfig,
-	tabWidth: 4,
-	singleQuote: true,
 };
--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,11 @@ import { useState } from 'react';
 export default function Home() {
 	const [pageStateManagement] = useState<'useImmer' | 'useState'>('useState');
 	const [globalStateManagement] = useState<'zstands' | 'unset'>('unset');
+	const eeee = {
+		a: 1,
+		b: 2,
+	};
+
 	return (
 		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
 			<main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
